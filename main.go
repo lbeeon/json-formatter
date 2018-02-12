ad0931c5344072664296993d3c3637244a11561a
Ben Wang (RD-TW) <ben_wang@trend.com.tw>
2018-02-13 00:43:49 +0800

    Support stdin and file

--- main.go
+++ main.go
@@ -3,15 +3,30 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
+var (
+	filePath = flag.String("file", "", "json file")
+)
+
+func init() {
+	flag.StringVar(filePath, "f", "", "json file")
+}
+
 func main() {
-	filePath := os.Args[1]
-	ioutil.ReadFile(filePath)
-	input, err := ioutil.ReadFile(filePath)
+	var err error
+	var input []byte
+	flag.Parse()
+	if len(*filePath) > 0 {
+		input, err = ioutil.ReadFile(*filePath)
+	} else {
+		input, err = ioutil.ReadAll(os.Stdin)
+	}
 	// input, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		log.Fatal("read data err:", err)
@@ -22,9 +37,12 @@ func main() {
 	if err != nil {
 		log.Fatal("json format err:", err)
 	}
-	err = ioutil.WriteFile(filePath, out.Bytes(), 0664)
+	io.Copy(os.Stdout, bytes.NewReader(out.Bytes()))
+	// if len(filePath) > 0 {
+	// 	err = ioutil.WriteFile(filePath, out.Bytes(), 0664)
+	// }
 
-	if err != nil {
-		log.Fatal("WriteFile err:", err)
-	}
+	// if err != nil {
+	// 	log.Fatal("WriteFile err:", err)
+	// }
 }
